67b3b117b62461dc900d07afddf76aa5cd4e38b4
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-11-29 14:21:55 +0700

    v1.4.1

--- socket.js
+++ socket.js
@@ -27,7 +27,7 @@ setInterval(()=> {
             const place = data.area
             
             if (place == 'Java, Indonesia' && Number(data.mag) > 2) {
-                fetch(`http://api.geonames.org/findNearbyWikipediaJSON?lat=${data.lintang}&lng=${data.bujur}&username=naufspace`, {
+                fetch(`https://secure.geonames.org/findNearbyWikipediaJSON?lat=${data.lintang}&lng=${data.bujur}&username=naufspace`, {
                     Method: 'GET',
                 }).then((response) => response.json()).then((data) => {
                     if (data.geonames[0] !== undefined) {
@@ -92,7 +92,7 @@ setInterval(()=> {
                             $('#history').append(' ' + "\n");
                             $('#history').append('[BMKG Real-Time Warning]' + "\n");
                             $('#history').append(unified + "\n");
-                            ping.volume = 0.5;
+                            ping.volume = 0.3;
                             ping.play();
                             $('#error-message').attr('placeholder', 'Loading...');
                             $('#error-message').html('');
@@ -117,7 +117,7 @@ setInterval(()=> {
                         $('#history').append(' ' + "\n");
                         $('#history').append('[BMKG Real-Time Warning]' + "\n");
                         $('#history').append(unified + "\n");
-                        ping.volume = 0.5;
+                        ping.volume = 0.3;
                         ping.play();
                         $('#error-message').attr('placeholder', 'Loading...');
                         $('#error-message').html('');
@@ -126,7 +126,32 @@ setInterval(()=> {
                         logger(bmkgLink)
                     }
                     console.log('Data geoname : ' + data)
-                })
+                }).catch((error) => {
+                    console.error('Error:', error);
+                    const unified = newDateTime + '\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                    const withoutMaps = newDateTime + '\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
+                    var history = [];
+                    if (localStorage.getItem("history") !== null) {
+                        var get = JSON.parse(localStorage.getItem("history"));
+                        for(var i = 0;i < get.length;i++){
+                        history.push(get[i])
+                        }
+                    }
+                    history.push(' ')
+                    history.push('[BMKG Real-Time Warning]')
+                    history.push(unified)
+                    localStorage.setItem("history", JSON.stringify(history));
+                    $('#history').append(' ' + "\n");
+                    $('#history').append('[BMKG Real-Time Warning]' + "\n");
+                    $('#history').append(unified + "\n");
+                    ping.volume = 0.3;
+                    ping.play();
+                    $('#error-message').attr('placeholder', 'Loading...');
+                    $('#error-message').html('');
+                    logger('[BMKG Real-Time Warning]')
+                    logger(withoutMaps)
+                    logger(bmkgLink)
+                });
             } else {
                 const unified = newDateTime + '\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
                 const withoutMaps = newDateTime + '\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
@@ -144,7 +169,7 @@ setInterval(()=> {
                 $('#history').append(' ' + "\n");
                 $('#history').append('[BMKG Real-Time Warning]' + "\n");
                 $('#history').append(unified + "\n");
-                ping.volume = 0.5;
+                ping.volume = 0.3;
                 ping.play();
                 $('#error-message').attr('placeholder', 'Loading...');
                 $('#error-message').html('');
@@ -152,71 +177,10 @@ setInterval(()=> {
                 logger(withoutMaps)
                 logger(bmkgLink)
             }
-            
-            console.log('==UPDATE==')
+            // console.log('==UPDATE==')
         }
-        console.log(currentTimeStamp)
-        console.log(timeStamp)
-        console.log('====')
+        // console.log(currentTimeStamp)
+        // console.log(timeStamp)
+        // console.log('====')
     });
-}, 500)
-
-
-
-// let currentTimeStamp = null
-// fetch(`https://bmkg-content-inatews.storage.googleapis.com/lastQL.json?t=${Date.now()}`, {
-//     Method: 'GET',
-// }).then((response) => response.json()).then((result) => {
-//     currentTimeStamp = result.features[0].properties.time
-// });
-// setInterval(()=> {
-//     fetch(`https://bmkg-content-inatews.storage.googleapis.com/lastQL.json?t=${Date.now()}`, {
-//         Method: 'GET',
-//     }).then((response) => response.json()).then((data) => {
-//         const timeStamp = data.features[0].properties.time
-//         if (currentTimeStamp != timeStamp) {
-//             currentTimeStamp = timeStamp
-//             const result = data.features[0]
-//             const maps = `https://google.com/maps/place/${result.geometry.coordinates[1]},${result.geometry.coordinates[0]}/@${result.geometry.coordinates[1]},${result.geometry.coordinates[0]},9.25z`
-//             const mag = 'M' + Number(result.properties.mag).toFixed(1)
-//             const depth = Number(Math.round(result.properties.depth)) + 'KM'
-//             const dateTime = result.properties.time
-//             const date = dateTime.split(' ')[0]
-//             const time = dateTime.split(' ')[1].split('.')[0]
-//             const newDateFormat = new Date(Date.UTC(date.split('-')[0], date.split('-')[1], date.split('-')[2], time.split(':')[0], time.split(':')[1], time.split(':')[2]));
-//             const newDateTime = newDateFormat.toLocaleString('id-ID')
-//             const place = result.properties.place
-//             const unified = newDateTime + ' : ' + mag + ' Kedalaman: ' + depth + ' Lokasi: ' + place + '. ' + maps;
-//             const withoutMaps = newDateTime + ' : ' + mag + ' Kedalaman: ' + depth + ' Lokasi: ' + place 
-//             var history = [];
-//             if (localStorage.getItem("history") !== null) {
-//                 var get = JSON.parse(localStorage.getItem("history"));
-//                 for(var i = 0;i < get.length;i++){
-//                 history.push(get[i])
-//                 }
-//             }
-//             history.push(' ')
-//             history.push('[BMKG Real-Time Warning]')
-//             history.push(unified)
-//             localStorage.setItem("history", JSON.stringify(history));
-//             $('#history').append(' ' + "\n");
-//             $('#history').append('[BMKG Real-Time Warning]' + "\n");
-//             $('#history').append(unified + "\n");
-//             ping.volume = 1;
-//             ping.play();
-//             $('#error-message').attr('placeholder', 'Loading...');
-//             $('#error-message').html('');
-//             logger('[BMKG Real-Time Warning]')
-//             logger(withoutMaps)
-//             logger(maps)
-//             // fetch(`http://api.geonames.org/findNearbyWikipediaJSON?lat=-6.84&lng=107.08&username=naufspace`, {
-//             //     Method: 'GET',
-//             // }).then((response) => response.json()).then((data) => {
-                
-//             // })
-//         }
-//         // console.log(currentTimeStamp)
-//         // console.log(timeStamp)
-//         // console.log('')
-//     });
-// }, 600)
+}, 1000)
