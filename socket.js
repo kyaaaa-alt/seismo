752211a1e681876d80e68cd555f7ae683b6a9135
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-11-30 22:05:27 +0700

    v1.5.5

--- socket.js
+++ socket.js
@@ -1,3 +1,10 @@
+const privateTrigger = document.getElementById('privateTrigger');
+var privateTriggerClicked = false;
+privateTrigger.addEventListener('click', function handleClick() {
+  privateTriggerClicked = true;
+  $('#riwayatlabel').html('Riwayat Alarm');
+});
+
 let currentTimeStamp = null
 let localDate = null
 var x2js = new X2JS();
@@ -42,64 +49,105 @@ setInterval(()=> {
                 fetch(`https://secure.geonames.org/findNearbyWikipediaJSON?lat=${data.lintang}&lng=${data.bujur}&username=naufspace`, {
                     Method: 'GET',
                 }).then((response) => response.json()).then((data) => {
-                    if (data.geonames[0] !== undefined) {
-                        const loc = data.geonames[0].title
-                        if (loc == 'Cianjur'
-                        || loc == 'Cianjur, Cianjur Regency' 
-                        || loc == 'Mount Gede' 
-                        || loc == 'Sindanglaya' 
-                        || loc == 'Cipanas' 
-                        || loc == 'Cianjur Regency' 
-                        || loc == 'Mount Pangrango' 
-                        || loc == 'Cibodas Botanical Garden' 
-                        || loc == 'Cipanas Palace' 
-                        || loc == 'Mayak (disambiguation)' 
-                        || loc == 'Mande' 
-                        || loc == 'Mount Parang' 
-                        || loc == 'Cipetir, Sukabumi' 
-                        || loc == 'Upper Cisokan Pumped Storage Power Plant' 
-                        || loc == 'Cirata Dam' 
-                        || loc == 'Cibodas' 
-                        || loc == 'Sukabumi' 
-                        || loc == 'Baros' 
-                        ) {
-                            if (Number(magnitude) > 2) {
-                                const unified = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
-                                const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
-                                var history = [];
-                                if (localStorage.getItem("history") !== null) {
-                                    var get = JSON.parse(localStorage.getItem("history"));
-                                    for(var i = 0;i < get.length;i++){
-                                    history.push(get[i])
+                    if (Object.prototype.hasOwnProperty.call(data, "geonames")) {
+                        if (data.geonames[0] !== undefined) {
+                            const loc = data.geonames[0].title
+                            if (loc == 'Cianjur'
+                            || loc == 'Cianjur, Cianjur Regency' 
+                            || loc == 'Mount Gede' 
+                            || loc == 'Sindanglaya' 
+                            || loc == 'Cipanas' 
+                            || loc == 'Cianjur Regency' 
+                            || loc == 'Mount Pangrango' 
+                            || loc == 'Cibodas Botanical Garden' 
+                            || loc == 'Cipanas Palace' 
+                            || loc == 'Mayak (disambiguation)' 
+                            || loc == 'Mande' 
+                            || loc == 'Mount Parang' 
+                            || loc == 'Cipetir, Sukabumi' 
+                            || loc == 'Upper Cisokan Pumped Storage Power Plant' 
+                            || loc == 'Cirata Dam' 
+                            || loc == 'Cibodas' 
+                            || loc == 'Sukabumi' 
+                            || loc == 'Baros' 
+                            ) {
+                                if (Number(magnitude) > 2) {
+                                    const unified = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                                    const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
+                                    var history = [];
+                                    if (localStorage.getItem("history") !== null) {
+                                        var get = JSON.parse(localStorage.getItem("history"));
+                                        for(var i = 0;i < get.length;i++){
+                                        history.push(get[i])
+                                        }
                                     }
+                                    history.push(' ')
+                                    history.push('[BMKG Real-Time Warning]')
+                                    history.push(unified)
+                                    localStorage.setItem("history", JSON.stringify(history));
+                                    $('#history').append(' ' + "\n");
+                                    $('#history').append('[BMKG Real-Time Warning]' + "\n");
+                                    $('#history').append(unified + "\n");
+                                    if (privateTriggerClicked) {
+                                        warning.volume = 1;
+                                        warning.play();
+                                    } else {
+                                        ping.volume = 0.2;
+                                        ping.play();
+                                    }
+                                    $('#warnBtn').show();
+                                    $('#error-message').attr('placeholder', 'Loading...');
+                                    $('#error-message').html('');
+                                    logger('[BMKG Real-Time Warning]')
+                                    logger(withoutMaps)
+                                    logger(bmkgLink)
+    
+                                    // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                                    // if (md.os() != 'iOS') {
+                                    //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                                    //         Method: 'GET',
+                                    //     }).then((response) => response.json()).then((data) => {
+                                    //         console.log(data, notificationData)
+                                    //     })
+                                    // }
+                                } else {
+                                    const unified = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                                    const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
+                                    var history = [];
+                                    if (localStorage.getItem("history") !== null) {
+                                        var get = JSON.parse(localStorage.getItem("history"));
+                                        for(var i = 0;i < get.length;i++){
+                                        history.push(get[i])
+                                        }
+                                    }
+                                    history.push(' ')
+                                    history.push('[BMKG Real-Time Warning]')
+                                    history.push(unified)
+                                    localStorage.setItem("history", JSON.stringify(history));
+                                    $('#history').append(' ' + "\n");
+                                    $('#history').append('[BMKG Real-Time Warning]' + "\n");
+                                    $('#history').append(unified + "\n");
+                                    ping.volume = 0.2;
+                                    ping.play();
+                                    $('#error-message').attr('placeholder', 'Loading...');
+                                    $('#error-message').html('');
+                                    logger('[BMKG Real-Time Warning]')
+                                    logger(withoutMaps)
+                                    logger(bmkgLink)
+    
+                                    // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                                    // if (md.os() != 'iOS') {
+                                    //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                                    //         Method: 'GET',
+                                    //     }).then((response) => response.json()).then((data) => {
+                                    //         console.log(data, notificationData)
+                                    //     })
+                                    // }
                                 }
-                                history.push(' ')
-                                history.push('[BMKG Real-Time Warning]')
-                                history.push(unified)
-                                localStorage.setItem("history", JSON.stringify(history));
-                                $('#history').append(' ' + "\n");
-                                $('#history').append('[BMKG Real-Time Warning]' + "\n");
-                                $('#history').append(unified + "\n");
-                                warning.volume = 1;
-                                warning.play();
-                                $('#warnBtn').show();
-                                $('#error-message').attr('placeholder', 'Loading...');
-                                $('#error-message').html('');
-                                logger('[BMKG Real-Time Warning]')
-                                logger(withoutMaps)
-                                logger(bmkgLink)
-
-                                // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                                // if (md.os() != 'iOS') {
-                                //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                                //         Method: 'GET',
-                                //     }).then((response) => response.json()).then((data) => {
-                                //         console.log(data, notificationData)
-                                //     })
-                                // }
+                                
                             } else {
-                                const unified = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
-                                const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Sekitar Cianjur & Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
+                                const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                                const withoutMaps = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
                                 var history = [];
                                 if (localStorage.getItem("history") !== null) {
                                     var get = JSON.parse(localStorage.getItem("history"));
@@ -114,15 +162,15 @@ setInterval(()=> {
                                 $('#history').append(' ' + "\n");
                                 $('#history').append('[BMKG Real-Time Warning]' + "\n");
                                 $('#history').append(unified + "\n");
-                                ping.volume = 0.3;
+                                ping.volume = 0.2;
                                 ping.play();
                                 $('#error-message').attr('placeholder', 'Loading...');
                                 $('#error-message').html('');
                                 logger('[BMKG Real-Time Warning]')
                                 logger(withoutMaps)
                                 logger(bmkgLink)
-
-                                // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+    
+                                // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
                                 // if (md.os() != 'iOS') {
                                 //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
                                 //         Method: 'GET',
@@ -131,7 +179,6 @@ setInterval(()=> {
                                 //     })
                                 // }
                             }
-                            
                         } else {
                             const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
                             const withoutMaps = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
@@ -149,14 +196,14 @@ setInterval(()=> {
                             $('#history').append(' ' + "\n");
                             $('#history').append('[BMKG Real-Time Warning]' + "\n");
                             $('#history').append(unified + "\n");
-                            ping.volume = 0.3;
+                            ping.volume = 0.2;
                             ping.play();
                             $('#error-message').attr('placeholder', 'Loading...');
                             $('#error-message').html('');
                             logger('[BMKG Real-Time Warning]')
                             logger(withoutMaps)
                             logger(bmkgLink)
-
+    
                             // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
                             // if (md.os() != 'iOS') {
                             //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
@@ -166,6 +213,8 @@ setInterval(()=> {
                             //     })
                             // }
                         }
+                        console.log('Data geoname : ')
+                        console.log(data)
                     } else {
                         const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
                         const withoutMaps = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
@@ -183,25 +232,15 @@ setInterval(()=> {
                         $('#history').append(' ' + "\n");
                         $('#history').append('[BMKG Real-Time Warning]' + "\n");
                         $('#history').append(unified + "\n");
-                        ping.volume = 0.3;
+                        ping.volume = 0.2;
                         ping.play();
                         $('#error-message').attr('placeholder', 'Loading...');
                         $('#error-message').html('');
                         logger('[BMKG Real-Time Warning]')
                         logger(withoutMaps)
                         logger(bmkgLink)
-
-                        // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                        // if (md.os() != 'iOS') {
-                        //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                        //         Method: 'GET',
-                        //     }).then((response) => response.json()).then((data) => {
-                        //         console.log(data, notificationData)
-                        //     })
-                        // }
                     }
-                    console.log('Data geoname : ')
-                    console.log(data)
+                    
                 }).catch((error) => {
                     console.error('Error:', error);
                     const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
@@ -220,7 +259,7 @@ setInterval(()=> {
                     $('#history').append(' ' + "\n");
                     $('#history').append('[BMKG Real-Time Warning]' + "\n");
                     $('#history').append(unified + "\n");
-                    ping.volume = 0.3;
+                    ping.volume = 0.2;
                     ping.play();
                     $('#error-message').attr('placeholder', 'Loading...');
                     $('#error-message').html('');
@@ -254,7 +293,7 @@ setInterval(()=> {
                 $('#history').append(' ' + "\n");
                 $('#history').append('[BMKG Real-Time Warning]' + "\n");
                 $('#history').append(unified + "\n");
-                ping.volume = 0.3;
+                ping.volume = 0.2;
                 ping.play();
                 $('#error-message').attr('placeholder', 'Loading...');
                 $('#error-message').html('');
