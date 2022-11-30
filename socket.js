b0abc9770fc9ed09bad8ce742116f14847f46f2d
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-11-30 12:25:52 +0700

    v1.5.2

--- socket.js
+++ socket.js
@@ -11,7 +11,7 @@ fetch(`https://bmkg-content-inatews.storage.googleapis.com/live30event.xml?t=${D
 setInterval(()=> {
     fetch(`https://bmkg-content-inatews.storage.googleapis.com/live30event.xml?t=${Date.now()}`, {
         Method: 'GET',
-    }).then((response) => response.text()).then((result) => {
+    }).then((response) => response.text()).then(async(result) => {
         const data = x2js.xml_str2json(result).Infogempa.gempa[0]
         const timeStamp = data.waktu
         if (currentTimeStamp != timeStamp) {
@@ -19,10 +19,11 @@ setInterval(()=> {
             const id = data.eventid
             const maps = `https://google.com/maps/place/${data.lintang},${data.bujur}/@${data.lintang},${data.bujur},9.25z`
             const bmkgLink = `https://inatews.bmkg.go.id/web/detail2?name=${data.eventid}`
+            const magnitude = data.mag
             const mag = 'M' + data.mag
             const depth = data.dalam + 'KM'
-            fetch(`https://bmkg-content-inatews.storage.googleapis.com/history.${id}.txt`, {
-                Method: 'POST',
+            await fetch(`https://bmkg-content-inatews.storage.googleapis.com/history.${id}.txt`, {
+                Method: 'GET',
             }).then((response) => response.text()).then((data) => {
                 const regex = /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/gi;
                 const match = data.match(regex)
@@ -33,8 +34,11 @@ setInterval(()=> {
                 localDate = m.local().locale('id').format(localFmt)
             })
             const place = data.area
-            
-            if (place == 'Java, Indonesia' && Number(data.mag) > 2) {
+
+            console.log('==UPDATE==')
+            console.log('localdate : ', localDate)
+
+            if (place == 'Java, Indonesia') {
                 fetch(`https://secure.geonames.org/findNearbyWikipediaJSON?lat=${data.lintang}&lng=${data.bujur}&username=naufspace`, {
                     Method: 'GET',
                 }).then((response) => response.json()).then((data) => {
@@ -59,39 +63,76 @@ setInterval(()=> {
                         || loc == 'Sukabumi' 
                         || loc == 'Baros' 
                         ) {
-                            const unified = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
-                            const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
-                            var history = [];
-                            if (localStorage.getItem("history") !== null) {
-                                var get = JSON.parse(localStorage.getItem("history"));
-                                for(var i = 0;i < get.length;i++){
-                                history.push(get[i])
+                            if (Number(magnitude) > 2) {
+                                const unified = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                                const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
+                                var history = [];
+                                if (localStorage.getItem("history") !== null) {
+                                    var get = JSON.parse(localStorage.getItem("history"));
+                                    for(var i = 0;i < get.length;i++){
+                                    history.push(get[i])
+                                    }
                                 }
-                            }
-                            history.push(' ')
-                            history.push('[BMKG Real-Time Warning]')
-                            history.push(unified)
-                            localStorage.setItem("history", JSON.stringify(history));
-                            $('#history').append(' ' + "\n");
-                            $('#history').append('[BMKG Real-Time Warning]' + "\n");
-                            $('#history').append(unified + "\n");
-                            warning.volume = 1;
-                            warning.play();
-                            $('#warnBtn').show();
-                            $('#error-message').attr('placeholder', 'Loading...');
-                            $('#error-message').html('');
-                            logger('[BMKG Real-Time Warning]')
-                            logger(withoutMaps)
-                            logger(bmkgLink)
+                                history.push(' ')
+                                history.push('[BMKG Real-Time Warning]')
+                                history.push(unified)
+                                localStorage.setItem("history", JSON.stringify(history));
+                                $('#history').append(' ' + "\n");
+                                $('#history').append('[BMKG Real-Time Warning]' + "\n");
+                                $('#history').append(unified + "\n");
+                                warning.volume = 1;
+                                warning.play();
+                                $('#warnBtn').show();
+                                $('#error-message').attr('placeholder', 'Loading...');
+                                $('#error-message').html('');
+                                logger('[BMKG Real-Time Warning]')
+                                logger(withoutMaps)
+                                logger(bmkgLink)
 
-                            const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                            if (md.os() != 'iOS') {
-                                fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                                    Method: 'GET',
-                                }).then((response) => response.json()).then((data) => {
-                                    console.log(data, notificationData)
-                                })
+                                // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                                // if (md.os() != 'iOS') {
+                                //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                                //         Method: 'GET',
+                                //     }).then((response) => response.json()).then((data) => {
+                                //         console.log(data, notificationData)
+                                //     })
+                                // }
+                            } else {
+                                const unified = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
+                                const withoutMaps = localDate + ' (Waktu Lokal)\n' + 'Cianjur / Sukabumi' + ', ' + mag + ', Kedalaman: ' + depth;
+                                var history = [];
+                                if (localStorage.getItem("history") !== null) {
+                                    var get = JSON.parse(localStorage.getItem("history"));
+                                    for(var i = 0;i < get.length;i++){
+                                    history.push(get[i])
+                                    }
+                                }
+                                history.push(' ')
+                                history.push('[BMKG Real-Time Warning]')
+                                history.push(unified)
+                                localStorage.setItem("history", JSON.stringify(history));
+                                $('#history').append(' ' + "\n");
+                                $('#history').append('[BMKG Real-Time Warning]' + "\n");
+                                $('#history').append(unified + "\n");
+                                ping.volume = 0.3;
+                                ping.play();
+                                $('#warnBtn').show();
+                                $('#error-message').attr('placeholder', 'Loading...');
+                                $('#error-message').html('');
+                                logger('[BMKG Real-Time Warning]')
+                                logger(withoutMaps)
+                                logger(bmkgLink)
+
+                                // const notificationData = localDate + ' (Waktu Lokal) | ' + 'Cianjur Sukabumi' + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                                // if (md.os() != 'iOS') {
+                                //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                                //         Method: 'GET',
+                                //     }).then((response) => response.json()).then((data) => {
+                                //         console.log(data, notificationData)
+                                //     })
+                                // }
                             }
+                            
                         } else {
                             const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
                             const withoutMaps = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth;
@@ -117,14 +158,14 @@ setInterval(()=> {
                             logger(withoutMaps)
                             logger(bmkgLink)
 
-                            const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                            if (md.os() != 'iOS') {
-                                fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                                    Method: 'GET',
-                                }).then((response) => response.json()).then((data) => {
-                                    console.log(data, notificationData)
-                                })
-                            }
+                            // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                            // if (md.os() != 'iOS') {
+                            //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                            //         Method: 'GET',
+                            //     }).then((response) => response.json()).then((data) => {
+                            //         console.log(data, notificationData)
+                            //     })
+                            // }
                         }
                     } else {
                         const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
@@ -151,14 +192,14 @@ setInterval(()=> {
                         logger(withoutMaps)
                         logger(bmkgLink)
 
-                        const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                        if (md.os() != 'iOS') {
-                            fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                                Method: 'GET',
-                            }).then((response) => response.json()).then((data) => {
-                                console.log(data, notificationData)
-                            })
-                        }
+                        // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                        // if (md.os() != 'iOS') {
+                        //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                        //         Method: 'GET',
+                        //     }).then((response) => response.json()).then((data) => {
+                        //         console.log(data, notificationData)
+                        //     })
+                        // }
                     }
                     console.log('Data geoname : ')
                     console.log(data)
@@ -188,14 +229,14 @@ setInterval(()=> {
                     logger(withoutMaps)
                     logger(bmkgLink)
 
-                    const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                    if (md.os() != 'iOS') {
-                        fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                            Method: 'GET',
-                        }).then((response) => response.json()).then((data) => {
-                            console.log(data, notificationData)
-                        })
-                    }
+                    // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                    // if (md.os() != 'iOS') {
+                    //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                    //         Method: 'GET',
+                    //     }).then((response) => response.json()).then((data) => {
+                    //         console.log(data, notificationData)
+                    //     })
+                    // }
                 });
             } else {
                 const unified = localDate + ' (Waktu Lokal)\n' + place + ', ' + mag + ', Kedalaman: ' + depth + ' ' + bmkgLink;
@@ -222,17 +263,15 @@ setInterval(()=> {
                 logger(withoutMaps)
                 logger(bmkgLink)
 
-                const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
-                if (md.os() != 'iOS') {
-                    fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
-                        Method: 'GET',
-                    }).then((response) => response.json()).then((data) => {
-                        console.log(data, notificationData)
-                    })
-                }
+                // const notificationData = localDate + ' (Waktu Lokal) | ' + place + ' | ' + mag.replace('.', ',') + ' | Kedalaman: ' + depth;
+                // if (md.os() != 'iOS') {
+                //     fetch(`https://eew.nauf.space/pusher_notif/${notificationData}/${id}/${localStorage.getItem('deviceId')}/bmkg`, {
+                //         Method: 'GET',
+                //     }).then((response) => response.json()).then((data) => {
+                //         console.log(data, notificationData)
+                //     })
+                // }
             }
-            console.log('==UPDATE==')
-            console.log('localdate ' + localDate)
         }
         // console.log(currentTimeStamp)
         // console.log(timeStamp)
